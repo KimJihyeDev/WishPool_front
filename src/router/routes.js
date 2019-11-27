c71e1a42729e72eaa58c820d7e26bd66fda2367d
HaeEunMyung <hayluna2@gmail.com>
2019-11-27 20:03:18 +0900

    temp_master 최초 merge

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,6 @@
 import CategoryList from '../components/category/CategoryList.vue'
 import ItemList from '../components/item/ItemList.vue'
 import ItemAdd from '../components/item/ItemAdd.vue'
-<<<<<<< HEAD
 import ItemModify from '../components/item/ItemModify.vue'
 // 웹소캣 연습용 컴포턴트 (나중에 삭제하기)
 import Temp from '../components/Temp.vue'
@@ -16,13 +15,4 @@ export default [
     
     
     
-=======
-import Temp from '../components/Temp.vue'
-
-export default [
-    { path:'/category/list', component:Category },
-    { path:'/item/list', component:ItemList },
-    { path:'/item/add', component:ItemAdd },
-    { path:'/temp', component:Temp}
->>>>>>> 40bb244fa1c1bdec9f5a887ced1b02fc52bd73a3
 ]
