5b7778aad5a0c86b6e63e12f6d71b5a83dd1430d
kjh <kim.jihye.dev@gmail.com>
2019-11-28 17:24:15 +0900

    commit

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,7 @@ import CategoryList from '../components/category/CategoryList.vue'
 import ItemList from '../components/item/ItemList.vue'
 import ItemAdd from '../components/item/ItemAdd.vue'
 import ItemModify from '../components/item/ItemModify.vue'
+import Entry from '../components/user/Entry'
 // 웹소캣 연습용 컴포턴트 (나중에 삭제하기)
 import Temp from '../components/Temp.vue'
 
@@ -11,6 +12,7 @@ export default [
     { path:'/item/list', component:ItemList},
     { path:'/item/add', component:ItemAdd},
     { path:'/item/modify',component:ItemModify},
+    { path:'/user/entry',component:Entry},
     { path:'/temp',component:Temp},
     
     
