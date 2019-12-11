4d04530541db92da52baa985f5d35bf91ceb06b0
kjh <kim.jihye.dev@gmail.com>
2019-12-11 14:34:51 +0900

    비밀번호 찾기 화면 수정

--- src/components/user/Login.vue
+++ src/components/user/Login.vue
@@ -33,6 +33,7 @@
                                             <div class="remember">
 
                                                 <div class="checkbox">
+                                                    
                                                     <label style="font-size:0.9rem">
                                                         <!-- <input name="optionsCheckboxes" type="checkbox"> -->
                                                         <span class="checkbox-material">
@@ -54,7 +55,7 @@
                                             <div class="or"></div>
 
                                             <a href="#" class="btn btn-lg bg-facebook full-width btn-icon-left" v-on:click="fbLogin">
-                                                <svg class="svg-inline--fa fa-facebook-f fa-w-10" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg><!-- <i class="fab fa-facebook-f" aria-hidden="true"></i> -->
+                                                <svg class="svg-inline--fa fa-facebook-f fa-w-10" aria-hidden="true" data-dismiss="modal" focusable="false" data-prefix="fab" data-icon="facebook-f" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg><!-- <i class="fab fa-facebook-f" aria-hidden="true"></i> -->
                                                 페이스북으로 로그인하기</a>
 
                                             <div class="or"></div>
